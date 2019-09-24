4f981632ff575b78c8c3f65e6bf001334f41628d
Michał <42038384+Averthel@users.noreply.github.com>
2019-09-24 23:58:22 +0200

    Initial commit

---
+++ src/main/java/pl/animeworld/Ave1Application.java
@@ -0,0 +1,13 @@
+package pl.animeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ave1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ave1Application.class, args);
+    }
+
+}
